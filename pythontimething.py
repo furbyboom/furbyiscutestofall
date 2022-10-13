d74bd819da90df0fc253b6a56ecdd1bbc3ab8c1e
furbyboom <115577747+furbyboom@users.noreply.github.com>
2022-10-13 15:41:51 -0700

    Update pythontimething.py

--- pythontimething.py
+++ pythontimething.py
@@ -1,11 +1,11 @@
 file_ = open(".zshrc")
-_file = open(".zshrc", "w")
 def read_wright(a, b):
     if a == True:
         return file_.read()
-    _file.write(b)
+    open(".zshrc", "w").write(b)
     return file_.read()
-data = read_wright(1, "")
+data = read_wright(True, "")
 data = data + "\n curl -O '" + input("url to extension") + "'"
 data = data + "\n sh " + input("name of extension")
+read_wrigh(False, data)
 exit()
